967bc4a01a8cdeeba4a2040157bd26ae0747fe23
jonathanhovland <jonathan.hovland@yahoo.com>
2019-01-10 11:43:05 -0700

    code cleanup

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,30 +1,17 @@
 import React, { Component } from "react"
-import { View, Text, TextInput, TouchableOpacity, Alert, Button ,StyleSheet ,StatusBar, Image} from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Alert, Button ,StyleSheet ,StatusBar, Image} from "react-native"
 import { Actions } from "react-native-router-flux"
-import t from "tcomb-form-native"
-
-const Form = t.form.Form
-
-const User = t.struct({
-    email: t.String,
-    username: t.String,
-    password: t.String,
-    make: t.String,
-    model: t.String,
-    year: t.String,
-    miles: t.String,
-  })
 
 class SignUp extends Component {
     static navigationOptions = {
-        headerStyle: { backgroundColor: '#4B8B9D' },
+        headerStyle: { backgroundColor: "#4B8B9D" },
         headerLeft: (
             <Button
-              onPress={() => Actions.login()}
-              title="LOG IN"
-              color="#FBF9F7"
+                onPress={() => Actions.login()}
+                title="LOG IN"
+                color="#FBF9F7"
             />
-          )
+        )
     }
     render() {
         return (
@@ -38,15 +25,13 @@ class SignUp extends Component {
                 autoCorrect={false} 
                 keyboardType='email-address' 
                 returnKeyType="next" 
-                placeholder='EMAIL' 
+                placeholder="EMAIL" 
                 placeholderTextColor="#AFD1D1"/>
             <TextInput style = {styles.input} 
                 autoCapitalize="none" 
                 onSubmitEditing={() => this.passwordInput.focus()} 
                 autoCorrect={false} 
-                keyboardType='email-address' 
-                returnKeyType="next" 
-                placeholder='USERNAME' 
+                placeholder="USERNAME" 
                 placeholderTextColor="#AFD1D1"/>
             <TextInput style = {styles.input}   
                 returnKeyType="go" ref={(input)=> this.passwordInput = input} 
@@ -57,36 +42,28 @@ class SignUp extends Component {
                 autoCapitalize="none" 
                 onSubmitEditing={() => this.passwordInput.focus()} 
                 autoCorrect={false} 
-                keyboardType='email-address' 
-                returnKeyType="next" 
-                placeholder='MAKE' 
+                placeholder="MAKE" 
                 placeholderTextColor="#AFD1D1"/>
             <TextInput style = {styles.input} 
                 autoCapitalize="none" 
                 onSubmitEditing={() => this.passwordInput.focus()} 
                 autoCorrect={false} 
-                keyboardType='email-address' 
-                returnKeyType="next" 
-                placeholder='MODEL' 
+                placeholder="MODEL" 
                 placeholderTextColor="#AFD1D1"/>
             <TextInput style = {styles.input} 
                 autoCapitalize="none" 
                 onSubmitEditing={() => this.passwordInput.focus()} 
                 autoCorrect={false} 
-                keyboardType='email-address' 
-                returnKeyType="next" 
-                placeholder='YEAR' 
+                placeholder="YEAR" 
                 placeholderTextColor="#AFD1D1"/>
             <TextInput style = {styles.input} 
                 autoCapitalize="none" 
                 onSubmitEditing={() => this.passwordInput.focus()} 
                 autoCorrect={false} 
-                keyboardType='email-address' 
-                returnKeyType="next" 
-                placeholder='MILES' 
+                placeholder="MILES" 
                 placeholderTextColor="#AFD1D1"/>
-            <TouchableOpacity style={styles.buttonContainerT} onPress={() => Actions.login()}>
-                <Text  style={styles.buttonTextT}>SIGN UP</Text>
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => Actions.login()}>
+                <Text  style={styles.buttonText}>SIGN UP</Text>
             </TouchableOpacity>
             </View>
         </View>
@@ -96,50 +73,38 @@ class SignUp extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#E5DEC0',
+        backgroundColor: "#E5DEC0",
         height: 1000,
         padding: 20,
-       },
-       inputContainer: {
+    },
+    inputContainer: {
         marginTop: 275,
-       },
-       input:{
+    },
+    input: {
            height: 40,
-           backgroundColor: '#4B8B9D',
+           backgroundColor: "#4B8B9D",
            marginTop: 10,
            padding: 10,
            color: '#FBF9F7',
-           textAlign: 'center'
-   
-       },
-       logo: {
-        position: 'absolute',
+           textAlign: "center"
+    },
+    logo: {
+        position: "absolute",
         width: 400,
         height: 100,
         marginTop: 110
     },
-       buttonContainer:{
-           backgroundColor: '#AFD1D1',
-           paddingVertical: 15,
-           marginBottom: 10
-       },
-       buttonContainerT:{
-           backgroundColor: '#FBF9F7',
+    buttonContainer: {
+           backgroundColor: "#FBF9F7",
            paddingVertical: 15,
            marginTop:12
-       },
-       buttonText:{
-           color: '#FBF9F7',
-           textAlign: 'center',
-           fontWeight: '500'
-       }, 
-       buttonTextT:{
-           color: '#76AFB8',
-           textAlign: 'center',
-           fontWeight: '500'
-    
-       }
-   })
+    },
+    buttonText: {
+           color: "#FBF9F7",
+           textAlign: "center",
+           fontWeight: "500"
+    }
+})
 
 
 export default SignUp

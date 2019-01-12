ea7e5fecc529b62c6ba712dc0a59a835f2e50715
jonathanhovland <jonathan.hovland@yahoo.com>
2019-01-12 13:48:11 -0700

    Created home page card for vehicle info

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,12 @@
-import React, { Component } from "react"
-import { View, Text, StyleSheet, Image, KeyboardAvoidingView, Button} from "react-native"
+// import React, { Component } from "react"
+// import {  StyleSheet, Left, Image} from "react-native"
 import { Actions } from "react-native-router-flux"
 import { ProgressCircle } from 'react-native-svg-charts'
+// import { Container, Header, Content, Card, CardItem, Text, Icon, Right, Button, Thumbnail, Body } from 'native-base';
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Text, Icon, Right, Button } from 'native-base';
+
+
 
 
 class Home extends Component {
@@ -26,32 +31,65 @@ class Home extends Component {
     
     render() {
         return (
-          <View style={styles.container}>
-          <Button
-          onPress={() => Actions.gas()}
-          title="GAs"
-          color="#FBF9F7"
-          />
-          <ProgressCircle
-                style={ { height: 200 } }
-                progress={ 0.7 }
-                progressColor={'#4B8B9D'}
-                startAngle={ -Math.PI * 0.8 }
-                endAngle={ Math.PI * 0.8 }
-            />
-          </View>
-        )
-    }
-}
-
-const styles = StyleSheet.create({
-  container: {
-      backgroundColor: '#E5DEC0',
-      flex: 1,
-      width: 375,
-  }
-})
-
-
-
-export default Home
+                <Container>
+        <Header />
+        <Content>
+          <Card>
+          <CardItem>
+              <Text>Info                  </Text>
+              <Text>      Due Date</Text>
+              <Right>
+                <Text>$    </Text>
+              </Right>
+             </CardItem>
+             <CardItem>
+              <Text>Payment              </Text>
+              <Text>(1/20/2019)</Text>
+              <Right>
+                <Text>$500</Text>
+              </Right>
+             </CardItem>
+             <CardItem>
+              <Text>Insurance          </Text>
+              <Text>  (1/20/2019)</Text>
+              <Right>
+                <Text>$110</Text>
+              </Right>
+             </CardItem>
+            <CardItem>
+              <Text>Registration          </Text>
+              <Text> (1/2020)</Text>
+              <Right>
+                <Text>$350</Text>
+              </Right>
+             </CardItem>
+           </Card>
+        </Content>
+        <Button
+        onPress={() => Actions.gas()}
+        title="GAs"
+        color="#FBF9F7"
+        />
+        <ProgressCircle
+              style={ { height: 200 } }
+              progress={ 0.7 }
+              progressColor={'#4B8B9D'}
+              startAngle={ -Math.PI * 0.8 }
+              endAngle={ Math.PI * 0.8 }
+          /> 
+              </Container>
+            )
+          }
+        }
+        
+        // const styles = StyleSheet.create({
+          //   container: {
+            //       backgroundColor: '#E5DEC0',
+            //       flex: 1,
+            //       width: 375,
+            //   }
+            // })
+            
+            
+            
+            export default Home

e0329a533f3e9f6ee94f80ff8bbceb5ceac04304
jonathanhovland <jonathan.hovland@yahoo.com>
2019-01-13 18:27:32 -0700

    Designed footer navigation on the home screen

--- components/screens/Home/HFooter.js
+++ components/screens/Home/HFooter.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 import { Actions } from "react-native-router-flux"
+import {  StyleSheet, Image} from "react-native"
+
 
 const HFooter = () => {
     return (
-      <Container>
+      <Container >
         <Content />
-        <Footer>
+        <Footer style={styles.container}>
           <FooterTab>
             <Button vertical
             onPress={() => Actions.gas()}>
-              <Icon name="speedometer" />
-              <Text>Gas</Text>
+              <Icon style={styles.white} name="speedometer" />
+              <Text style={styles.white}>Gas</Text>
             </Button>
             <Button vertical
-            onPress={() => Actions.maint()}>
-              <Icon active name="car" />
-              <Text>Maint.</Text>
+            onPress={() => Actions.maint()}
+            >
+              <Icon  style={styles.white} active name="car" />
+              <Text style={styles.white}>Maint.</Text>
             </Button>
             <Button vertical
             onPress={() => Actions.data()}>
-              <Icon name="pie" />
-              <Text>Data</Text>
+              <Icon style={styles.white} name="pie" />
+              <Text style={styles.white}>Data</Text>
             </Button>
           </FooterTab>
         </Footer>
       </Container>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+          backgroundColor: '#4B8B9D',
+    },
+    white: {
+      color: '#FBF9F7'
+   }
+
+    })
 
   export default HFooter

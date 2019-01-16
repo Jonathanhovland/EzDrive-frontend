1691d45b87d4f6a63658843701aecdabbd69ca18
jonathanhovland <jonathan.hovland@yahoo.com>
2019-01-16 07:17:39 -0700

    Updated gas history cards

--- components/screens/Gas/GasHistory.js
+++ components/screens/Gas/GasHistory.js
@@ -9,17 +9,14 @@ const GasHistory = ({gasHistory}) => {
   return (   
     <Container key={i} style={styles.container}>
       <Content>
-        <Card >
-          <CardItem style={styles.cardContainer}>
-            <Text style={styles.cardText}>History</Text>
-          </CardItem>
+        <Card style={styles.card}>
           <CardItem style={styles.cardContainer}>
             <Left>
               <Text style={styles.cardText}>{gas.date}</Text>
             </Left>
-            <Text style={styles.cardText}>{gas.gas_amount}</Text>
+            <Text style={styles.cardText}>{gas.gas_amount} gallons</Text>
             <Right>
-              <Text style={styles.cardText}>{gas.cost}</Text>
+              <Text style={styles.cardText}>${gas.cost}</Text>
             </Right>
             </CardItem>
           </Card>
@@ -31,14 +28,18 @@ const GasHistory = ({gasHistory}) => {
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: "#E5DEC0"
+      backgroundColor: "#E5DEC0",
+      marginBottom: -175
 
+  },
+  card : {
+    marginBottom: 0
   },
   cardContainer: {
     backgroundColor: "#AFD1D1",
     justifyContent: "center",
-    alignItems: "center"  
-
+    alignItems: "center",
+    marginBottom: -40
   },    
   cardText:{
     color: "#FBF9F7",
